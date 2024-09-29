617a5f81f67190f0c2e521ad206b3755c68a7bad
sdr3a34f <124947212+sdr3a34f@users.noreply.github.com>
2024-09-29 13:04:55 +0800

    Update diy.sh

--- cm311/diy.sh
+++ cm311/diy.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+# Default IP
+sed -i 's/192.168.1.1/192.168.10.99/g' package/base-files/files/bin/config_generate
+
+# Remove packages
+rm -rf feeds/luci/applications/luci-app-passwall
+
+# Add packages
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall-packages
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/openwrt-passwall2
+git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
